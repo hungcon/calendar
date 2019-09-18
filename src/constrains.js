b36c48f9e9911210d96d50c88cbdab88314afda3
Hungpv1883 <hungcon.5070@gmail.com>
2019-09-18 13:49:30 +0700

    add constrain

--- src/constrains.js
+++ src/constrains.js
@@ -1,17 +1,18 @@
 var constraints = {
     email: {
-      presence: true,
+      presence: {
+        "allowEmpty": false
+      },
       exclusion: {
         within: ["nicklas"],
         message: "'%{value}' is not allowed"
       },
-      format: {
-        pattern : /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/,
-        message : "not is a valid email",
-      }
+      email: true
     },
     password: {
-      presence: true,
+        presence: {
+            "allowEmpty": false
+        },
       length: {
         minimum: 6,
         message: "must be at least 6 characters"
