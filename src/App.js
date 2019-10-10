f1c4db521e91be0ada7a940834ef2662418d394b
HungPv <hungcon.5070@gmail.com>
2019-10-10 23:07:24 +0700

    abc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+
 import { Route , BrowserRouter } from "react-router-dom";
 import PrivateRoute from './Component/Routes/PrivateRoute';
 import Bar from './Component/Layout/Bar';
@@ -8,12 +8,12 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
+     
         <BrowserRouter basename=''>
             <Route path="/" exact component={Bar} /> 
             <PrivateRoute path="/home" exact component={Home} /> 
         </BrowserRouter >
-      </Router>
+    
 
     );
   }
