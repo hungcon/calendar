16bed7110a4863e39c33ac69908e985f8fca8155
HungPv <hungcon.5070@gmail.com>
2019-10-10 22:34:31 +0700

    fix router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
-import { Route , BrowserRouter, Switch } from "react-router-dom";
+import { Route , BrowserRouter } from "react-router-dom";
 import PrivateRoute from './Component/Routes/PrivateRoute';
 import Bar from './Component/Layout/Bar';
 import Home from './Component/Layout/Home';
@@ -9,10 +9,10 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Switch basename={window.location.pathname || ''}>
+        <BrowserRouter basename={window.location.pathname || ''} forceRefresh={true}>
             <Route path="/" exact component={Bar} /> 
             <PrivateRoute path="/home" exact component={Home} /> 
-        </Switch >
+        </BrowserRouter >
       </Router>
 
     );
